9fb33d6ba576df4d47c7d1796aad76653148f08d
KOSTYANKYRYCHENKO006 <kyrychenkokostyan@gmail.com>
2023-06-08 17:52:13 +0200

    changes: Country.js added some information about countries but they didn't add to our page but they are in our file Country.js

--- src/pages/CountriesPage/components/Body/components/CountryList/components/Country/Country.js
+++ src/pages/CountriesPage/components/Body/components/CountryList/components/Country/Country.js
@@ -2,7 +2,12 @@ export const Country = ({ currentCountry }) => {
     return (
         <>
             <h3>{currentCountry.name.common}</h3>
+            <div>capital: {currentCountry.capital}</div>
+            <div>borders: {currentCountry.borders}</div>
             <div>area: {currentCountry.area}km</div>
+            <div>region: {currentCountry.region}</div>
+            <div>subregion: {currentCountry.subregion}</div>
+            <div>flags: {currentCountry.flags}</div>
         </>
     )
 }
