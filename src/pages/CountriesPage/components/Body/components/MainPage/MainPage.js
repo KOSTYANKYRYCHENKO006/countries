55e99cae8fe0142580028ff9704bdca8fd10d5e9
DeskorED <rfpfywtdf29@gmail.com>
2023-07-04 22:28:26 +0200

    refactoring

--- src/pages/CountriesPage/components/Body/components/MainPage/MainPage.js
+++ src/pages/CountriesPage/components/Body/components/MainPage/MainPage.js
@@ -21,6 +21,7 @@ export const MainPage = () => {
                 <div className="information__header">
                     <h2>Новости про страны</h2>
                 </div>
+                <div className="information__content">
                     <div className="sidebar_container">
                         <div className="infos info_ukraine">
                             <img src={image} alt=""></img>
