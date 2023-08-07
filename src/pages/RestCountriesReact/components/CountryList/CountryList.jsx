083c98fa237ca56d0e4865e0fb38ddfa16a2bd1a
DeskorED <rfpfywtdf29@gmail.com>
2023-08-07 14:12:28 +0200

    refactor: modify CountryList, Search

--- src/pages/RestCountriesReact/components/CountryList/CountryList.jsx
+++ src/pages/RestCountriesReact/components/CountryList/CountryList.jsx
@@ -1,19 +1,33 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CountryListContext } from "../../RestCountriesReact"
 
 import { Country } from "./components/Country"
+import { Search } from "../Search"
 
 import "./style.scss"
 
 export const CountryList = () => {
 
     let countryList = useContext(CountryListContext);
+    let [searchResult, setSearchResult] = useState('')
+
+    let filteredCountryList = countryList.filter(
+        country => country
+            ?.data
+            ?.name
+            ?.official
+            ?.toLowerCase()
+            ?.includes(searchResult?.toLowerCase())
+    )
 
     return (
-        <div className="country-list">
-            {
-                countryList && countryList.map((country, index) => <Country key={index} currentCountry={country?.data} />)
-            }
+        <div>
+            <Search value={searchResult} setValue={setSearchResult} />
+            <div className="country-list">
+                {
+                    filteredCountryList && filteredCountryList.map((country, index) => <Country key={index} currentCountry={country?.data} />)
+                }
+            </div>
         </div>
     )
 }
--- src/pages/RestCountriesReact/components/Search/Search.jsx
+++ src/pages/RestCountriesReact/components/Search/Search.jsx
@@ -1,14 +1,16 @@
-export const Search = () => {
+import "./style.scss"
+
+export const Search = ({value, setValue}) => {
     return(
         <div>
             <div className="form">
                 <form className="search_form">
                     <input
                     type="text"
-                    placeholder="search_placeholder"
+                    placeholder="Введите искомую страну"
                     className="search_input"
-                    value={searchTerm}
-                    onChange={handlesearch}
+                    value={value}
+                    onChange={(event) => setValue(event.target.value) }
                     />
                 </form>
             </div>
